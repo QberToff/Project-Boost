cffab14b0f895edcfb6424c60a637937f129780e
QberToff <74160641+QberToff@users.noreply.github.com>
2021-01-10 00:19:21 +0300

    Added colider for rocket, added script Rocket.cs

---
+++ Assets/Scripts/Rocket.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();   
+    }
+
+    void ProcessInput()
+    {
+        if(Input.GetKey(KeyCode.Space))
+        {
+            Debug.Log("Space pressed");
+        }
+        if(Input.GetKey(KeyCode.A))
+        {
+            Debug.Log("Left pressed");
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            Debug.Log("Right pressed");
+        }
+    }
+
+}
