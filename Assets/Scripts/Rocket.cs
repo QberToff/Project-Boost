cfacdf1e3b7c4df2b841ba9df73aedb18cf0eb3e
QberToff <74160641+QberToff@users.noreply.github.com>
2021-01-17 19:19:17 +0300

    addd scene managment and changed 1 level

--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -1,37 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Rocket : MonoBehaviour
 {
-    Rigidbody rigidBody;
-    [SerializeField] float thrust = 1;
-    [SerializeField] float rcsThrust = 100f;
-    AudioSource audioSource;
-    
+
     // Start is called before the first frame update
     void Start()
     {
-        rigidBody = GetComponent<Rigidbody>();
-        audioSource = GetComponent<AudioSource>();
-       
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        Thrust();
-        Rotate();   
-    }
 
+    }
 
     private void OnCollisionEnter(Collision collision)
     {
         switch (collision.gameObject.tag)
-            {
+        {
             case "Friendly":
                 {
                     Debug.Log("Ok");
+                    break;
+                }
+            case "Finished":
+                {
+
+                    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
                     break;
                 }
             default:
@@ -39,45 +39,7 @@ public class Rocket : MonoBehaviour
                     Debug.Log("Dead");
                     break;
                 }
-            }
-    }
-
-    void Rotate()
-    {
-        rigidBody.freezeRotation = true;
-        float rotationThisFrame = rcsThrust * Time.deltaTime;
-
-        if (Input.GetKey(KeyCode.A))
-        {
-          
-           
-            transform.Rotate(Vector3.back * rotationThisFrame);
-        }
-        else if (Input.GetKey(KeyCode.D))
-        {
-            
-            transform.Rotate(Vector3.forward * rotationThisFrame);
-        }
-
-        rigidBody.freezeRotation = false;
-    }
-
-     public void Thrust()
-    {
-        if (Input.GetKey(KeyCode.Space))
-        {
-            
-            rigidBody.AddRelativeForce(Vector3.up * thrust * Time.deltaTime);
-            if (!audioSource.isPlaying)
-            {
-                audioSource.Play();
-            }
-
-        }
-
-        else
-        {
-            audioSource.Stop();
         }
     }
+
 }
