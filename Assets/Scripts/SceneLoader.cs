9e6c7ae671a935ad6d14c60cb039d8101a1c9fcc
QberToff <74160641+QberToff@users.noreply.github.com>
2021-01-19 21:05:24 +0300

    added bar

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadFirstLevel()
+    {
+        SceneManager.LoadScene("Level 1");
+    }
+
+    public void LoadRules()
+    {
+        SceneManager.LoadScene("Rules");
+    }
+
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+}
