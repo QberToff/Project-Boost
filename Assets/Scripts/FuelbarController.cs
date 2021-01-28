b9339ea7ad099a04026594f6723764e3edf9ec07
QberToff <74160641+QberToff@users.noreply.github.com>
2021-01-28 18:14:28 +0300

    added HUD slider for debug

--- Assets/Scripts/FuelbarController.cs
+++ Assets/Scripts/FuelbarController.cs
@@ -8,19 +8,23 @@ public class FuelbarController : MonoBehaviour
     [SerializeField] float startFuel;
     [SerializeField] Rocket rocket;
     [SerializeField] Image fuelbar;
+    [SerializeField] Text text;
+    [SerializeField] Slider slider;
     //float testValue = 0.5f;
 
     private void Start()
     {
         startFuel = rocket.GetStartFuel();
+        slider.maxValue = startFuel;
     }
     public void Update()
     {
-        
+
+        slider.value = rocket.GetFuel();
         fuelbar.fillAmount = rocket.GetFuel() / startFuel;
+        text.text = rocket.GetFuel().ToString();
+
+
     }
-
-
-   
 
 }
